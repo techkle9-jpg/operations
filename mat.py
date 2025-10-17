d282c42377829869eecf88ce43e5ac865e6112ae
techkle9-jpg <techkle9@gmail.com>
2025-10-17 09:33:03 +0530

    Create mat.py

---
+++ mat.py
@@ -0,0 +1,10 @@
+try:
+    a= float(input("enter first number:"))
+    b= float(input("enter second number:"))
+
+print(f"addition:{a+b}")
+print(f"substraction:{a-b}")
+
+Except ValueError:
+
+print("enter a valid number")
